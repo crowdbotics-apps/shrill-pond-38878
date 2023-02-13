bb47cde40dd6a49409a1c42bea43d9a5158a7c02
Crowdbotics <team@crowdbotics.com>
2023-02-13 16:49:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-pond-38878",
+  url: "https://shrill-pond-38878.botics.co",
+  api: "https://shrill-pond-38878.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
